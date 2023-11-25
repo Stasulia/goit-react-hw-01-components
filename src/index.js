389c205c8c06523dd333a073fcc033123057a93e
Stasulia <134652473+Stasulia@users.noreply.github.com>
2023-11-25 21:59:52 +0200

    return BrowserRouter

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { App } from 'components/App';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter basename="/goit-react-hw-01-components">
+  <React.StrictMode>
     <App />
-  </BrowserRouter>
+  </React.StrictMode>
 );
